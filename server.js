22e9ef02d8a6fbda3ce35fe25b90b6f257bb528f
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-04-25 11:16:53 -0700

    handlebars template in index.html, scripts wired up

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const cors = require('cors');
 const pg = require('pg');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
@@ -22,7 +23,114 @@ app.get('/api/v1/books', (req, res) => {
     .catch(console.error);
 });
 
+app.post('/books', (request, response) => {
+  client.query(
+    `INSERT INTO
+    books(author, title, isbn, image_url, description)
+    VALUES ($1, $2, $3, $4, $5);
+    `,
+    [
+      request.body.author,
+      request.body.title,
+      request.body.isbn,
+      request.body.image_url,
+      request.body.description
+    ]
+  )
+    .then(function () {
+      response.send('insert complete');
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+});
+
+app.put('/books/:id', (request, response) => {
+  client.query(
+    `UPDATE books
+    SET 
+    author=$1, title=$2, isbn=$3, image_url=$4, description=$5
+    WHERE book_id=$6;
+    `,
+    [
+      request.body.author,
+      request.body.title,
+      request.body.isbn,
+      request.body.image_url,
+      request.body.description,
+      request.params.id
+    ]
+  )
+    .then(() => {
+      response.send('update complete');
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
+
+app.delete('/books/:id', (request, response) => {
+  client.query(
+    `DELETE FROM books WHERE book_id=$1;`,
+    [request.params.id]
+  )
+    .then(() => {
+      response.send('Delete complete');
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
+
+app.delete('/books', (request, response) => {
+  client.query(
+    'DELETE FROM books'
+  )
+    .then(() => {
+      response.send('Delete complete');
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
+
+function loadBooks() {
+  client.query('SELECT COUNT(*) FROM books')
+    .then(result => {
+      if (!parseInt(result.rows[0].count)) {
+        fs.readFile('../book-list-client/data/books.json', 'utf8', (err, fd) => {
+          JSON.parse(fd).forEach(ele => {
+            client.query(`
+            INSERT INTO
+            books(author, title, isbn, image_url, description)
+            VALUES ($1, $2, $3, $4, $5);
+          `,
+              [ele.author, ele.title, ele.isbn, ele.image_url, ele.description]
+            );
+          });
+        });
+      }
+    });
+}
+
+loadDB();
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+function loadDB() {
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    books (
+      book_id SERIAL PRIMARY KEY,
+      author TEXT NOT NULL,
+      title VARCHAR(255) NOT NULL,
+      isbn VARCHAR(255),
+      image_url VARCHAR(255),
+      description TEXT NOT NULL
+    );`
+  )
+    .then(loadBooks)
+    .catch(console.error);
+}
 
 /*
 CREATE DATABASE book_app;
