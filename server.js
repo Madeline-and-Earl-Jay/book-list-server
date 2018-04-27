439fe397c120f532466b087006c65531247be917
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-04-26 18:57:11 -0700

    sql query for add

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/api/v1/books/:id', (req, res) => {
       res.status(500).send('error');
     });
 });
-app.post('/books', (request, response) => {
+app.post('/api/v1/books', (request, response) => {
   client.query(
     `INSERT INTO
     books(author, title, isbn, image_url, description)
@@ -45,87 +45,85 @@ app.post('/books', (request, response) => {
       request.body.description
     ]
   )
-    .then(function () {
-      response.send('insert complete');
-    })
-    .catch(function (err) {
+    .then(results => response.json(results))
+    .catch(err => {
       console.error(err);
+      res.sendStatus(500).send("Error");
     });
-});
 
-app.put('/books/:id', (request, response) => {
-  client.query(
-    `UPDATE books
+  app.put('/books/:id', (request, response) => {
+    client.query(
+      `UPDATE books
     SET 
     author=$1, title=$2, isbn=$3, image_url=$4, description=$5
     WHERE book_id=$6;
     `,
-    [
-      request.body.author,
-      request.body.title,
-      request.body.isbn,
-      request.body.image_url,
-      request.body.description,
-      request.params.id
-    ]
-  )
-    .then(() => {
-      response.send('update complete');
-    })
-    .catch(err => {
-      console.error(err);
-    });
-});
+      [
+        request.body.author,
+        request.body.title,
+        request.body.isbn,
+        request.body.image_url,
+        request.body.description,
+        request.params.id
+      ]
+    )
+      .then(() => {
+        response.send('update complete');
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
 
-app.delete('/books/:id', (request, response) => {
-  client.query(
-    `DELETE FROM books WHERE book_id=$1;`,
-    [request.params.id]
-  )
-    .then(() => {
-      response.send('Delete complete');
-    })
-    .catch(err => {
-      console.error(err);
-    });
-});
+  app.delete('/books/:id', (request, response) => {
+    client.query(
+      `DELETE FROM books WHERE book_id=$1;`,
+      [request.params.id]
+    )
+      .then(() => {
+        response.send('Delete complete');
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
 
-app.delete('/books', (request, response) => {
-  client.query(
-    'DELETE FROM books'
-  )
-    .then(() => {
-      response.send('Delete complete');
-    })
-    .catch(err => {
-      console.error(err);
-    });
-});
+  app.delete('/books', (request, response) => {
+    client.query(
+      'DELETE FROM books'
+    )
+      .then(() => {
+        response.send('Delete complete');
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
 
-function loadBooks() {
-  client.query('SELECT COUNT(*) FROM books')
-    .then(result => {
-      if (!parseInt(result.rows[0].count)) {
-        fs.readFile('../book-list-client/data/books.json', 'utf8', (err, fd) => {
-          JSON.parse(fd).forEach(ele => {
-            client.query(`
+  function loadBooks() {
+    client.query('SELECT COUNT(*) FROM books')
+      .then(result => {
+        if (!parseInt(result.rows[0].count)) {
+          fs.readFile('../book-list-client/data/books.json', 'utf8', (err, fd) => {
+            JSON.parse(fd).forEach(ele => {
+              client.query(`
             INSERT INTO
             books(author, title, isbn, image_url, description)
             VALUES ($1, $2, $3, $4, $5);
           `,
-            [ele.author, ele.title, ele.isbn, ele.image_url, ele.description]
-            );
+                [ele.author, ele.title, ele.isbn, ele.image_url, ele.description]
+              );
+            });
           });
-        });
-      }
-    });
-}
+        }
+      });
+  }
 
-loadDB();
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+  loadDB();
+  app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
-function loadDB() {
-  client.query(`
+  function loadDB() {
+    client.query(`
     CREATE TABLE IF NOT EXISTS
     books(
       book_id SERIAL PRIMARY KEY,
@@ -135,10 +133,10 @@ function loadDB() {
       image_url VARCHAR(255),
       description TEXT NOT NULL
     );`
-  )
-    .then(loadBooks)
-    .catch(console.error);
-}
+    )
+      .then(loadBooks)
+      .catch(console.error);
+  }
 
 /*
 CREATE DATABASE book_app;
