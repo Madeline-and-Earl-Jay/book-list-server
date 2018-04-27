c3dfbae5a834643e643c0358b369e2362a81e1a3
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-04-27 08:56:12 -0700

    added closing bracket to app.post function

--- server.js
+++ server.js
@@ -48,82 +48,82 @@ app.post('/api/v1/books', (request, response) => {
     .then(results => response.json(results))
     .catch(err => {
       console.error(err);
-      res.sendStatus(500).send("Error");
+      response.sendStatus(500).send("Error");
     });
-
-  app.put('/books/:id', (request, response) => {
-    client.query(
-      `UPDATE books
+});
+app.put('/books/:id', (request, response) => {
+  client.query(
+    `UPDATE books
     SET 
     author=$1, title=$2, isbn=$3, image_url=$4, description=$5
     WHERE book_id=$6;
     `,
-      [
-        request.body.author,
-        request.body.title,
-        request.body.isbn,
-        request.body.image_url,
-        request.body.description,
-        request.params.id
-      ]
-    )
-      .then(() => {
-        response.send('update complete');
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  });
+    [
+      request.body.author,
+      request.body.title,
+      request.body.isbn,
+      request.body.image_url,
+      request.body.description,
+      request.params.id
+    ]
+  )
+    .then(() => {
+      response.send('update complete');
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
 
-  app.delete('/books/:id', (request, response) => {
-    client.query(
-      `DELETE FROM books WHERE book_id=$1;`,
-      [request.params.id]
-    )
-      .then(() => {
-        response.send('Delete complete');
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  });
+app.delete('/books/:id', (request, response) => {
+  client.query(
+    `DELETE FROM books WHERE book_id=$1;`,
+    [request.params.id]
+  )
+    .then(() => {
+      response.send('Delete complete');
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
 
-  app.delete('/books', (request, response) => {
-    client.query(
-      'DELETE FROM books'
-    )
-      .then(() => {
-        response.send('Delete complete');
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  });
+app.delete('/books', (request, response) => {
+  client.query(
+    'DELETE FROM books'
+  )
+    .then(() => {
+      response.send('Delete complete');
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
 
-  function loadBooks() {
-    client.query('SELECT COUNT(*) FROM books')
-      .then(result => {
-        if (!parseInt(result.rows[0].count)) {
-          fs.readFile('../book-list-client/data/books.json', 'utf8', (err, fd) => {
-            JSON.parse(fd).forEach(ele => {
-              client.query(`
+function loadBooks() {
+  client.query('SELECT COUNT(*) FROM books')
+    .then(result => {
+      if (!parseInt(result.rows[0].count)) {
+        fs.readFile('../book-list-client/data/books.json', 'utf8', (err, fd) => {
+          JSON.parse(fd).forEach(ele => {
+            client.query(`
             INSERT INTO
             books(author, title, isbn, image_url, description)
             VALUES ($1, $2, $3, $4, $5);
           `,
-                [ele.author, ele.title, ele.isbn, ele.image_url, ele.description]
-              );
-            });
+              [ele.author, ele.title, ele.isbn, ele.image_url, ele.description]
+            );
           });
-        }
-      });
-  }
+        });
+      }
+    });
+}
 
-  loadDB();
-  app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+loadDB();
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
-  function loadDB() {
-    client.query(`
+function loadDB() {
+  client.query(`
     CREATE TABLE IF NOT EXISTS
     books(
       book_id SERIAL PRIMARY KEY,
@@ -133,10 +133,10 @@ app.post('/api/v1/books', (request, response) => {
       image_url VARCHAR(255),
       description TEXT NOT NULL
     );`
-    )
-      .then(loadBooks)
-      .catch(console.error);
-  }
+  )
+    .then(loadBooks)
+    .catch(console.error);
+}
 
 /*
 CREATE DATABASE book_app;
@@ -144,5 +144,4 @@ CREATE DATABASE book_app;
 CREATE TABLE books;
 
 CREATE TABLE books ( book_id SERIAL PRIMARY KEY, author TEXT, title TEXT, isbn VARCHAR(20), image_url VARCHAR(255), description TEXT );
-
 */
